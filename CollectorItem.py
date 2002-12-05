0987b5577bd5600ef6d1c79b8cc65fd2b5fd4245
bdelbosc <devnull@localhost>
2002-12-05 15:25:04 +0000

    securize view method

--- CollectorItem.py
+++ CollectorItem.py
@@ -4,11 +4,13 @@
 ### import
 from Globals import InitializeClass
 from OFS.SimpleItem import SimpleItem
-
+from AccessControl import ClassSecurityInfo
+from Products.CMFCore.CMFCorePermissions import ModifyPortalContent
 
 ### class
 class CollectorItem(SimpleItem):
-    "CollectorItem class"
+    security = ClassSecurityInfo()    
+
     meta_type = 'CollectorItem'
     manage_options = (
         { 'label':'View', 'action':'view' },
@@ -19,7 +21,8 @@ class CollectorItem(SimpleItem):
         self.id = id
         self.data = form
         self._p_changed = 1
-
+        
+    security.declareProtected(ModifyPortalContent, 'view')
     def view( self ):
         "view"
         return str( self.data )
