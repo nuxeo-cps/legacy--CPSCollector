bb3f03580ae288a6d7ef3ec32ee2604af32d252d
epietrig <devnull@localhost>
2003-10-21 13:53:23 +0000

    returns appropriate edit form depending on collector doc type

--- skins/cps/Form_moveFieldUp.py
+++ skins/cps/Form_moveFieldUp.py
@@ -3,4 +3,8 @@ doc = context.getContent()
 
 f_id = context.REQUEST.form.get('f_id')
 doc.move_fields(f_id, 'up')
-return context.Form_editForm()
+
+if doc.meta_type == 'Quiz Document':
+    return context.Form_editQuizForm()
+else:
+    return context.Form_editForm()
